62e475f040de742c85e8b790f2629fa591d745ba
dany <danychen.me@gmail.com>
2024-11-21 19:33:24 -0800

    tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+import colors, { pink } from "tailwindcss/colors";
+
 export default {
-  content: ['./index.html', 'src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: {
+        ...colors.gray,
+        DEFAULT: colors.gray["600"],
+      },
+      sky: {
+        ...colors.sky,
+        DEFAULT: colors.sky["600"],
+      },
+      red: {
+        ...colors.red,
+        DEFAULT: colors.red["600"],
+      },
+    },
     extend: {},
   },
   plugins: [],
