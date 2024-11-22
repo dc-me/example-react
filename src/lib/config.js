476971c2568148f89c5bed1e31124559fdd22e82
dany <danychen.me@gmail.com>
2024-11-22 09:39:04 -0800

    use env for axios base url

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,5 +2,5 @@
  * Common configuration
  */
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASEURL;
 axios.defaults.withCredentials = true;
