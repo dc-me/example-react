b8c68e57576353563c9894888c43b1239bcdc0f5
dany <danychen.me@gmail.com>
2024-11-21 19:36:09 -0800

    add login route and use config js

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.css';
-import Movie from './components/movie';
-import FashionBlog from './components/fashion-blog';
-import NewsPage from './components/news';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./lib/config";
+import "./index.css";
+import Movie from "./components/movie";
+import FashionBlog from "./components/fashion-blog";
+import NewsPage from "./components/news";
+import LoginPage from "./components/user/LoginPage";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/movie" element={<Movie />} />
         <Route path="/fashion-blog" element={<FashionBlog />} />
         <Route path="/news" element={<NewsPage />} />
+        <Route path="/users/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
