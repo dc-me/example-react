7c2653b8de0f4e37da5ddad374a6f6cef418f05c
dany <danychen.me@gmail.com>
2024-11-22 14:47:49 -0800

    add signup and chat route

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import Movie from "./components/movie";
 import FashionBlog from "./components/fashion-blog";
 import NewsPage from "./components/news";
 import LoginPage from "./components/user/LoginPage";
+import ChatPage from "./components/chat";
+import RegisterPage from "./components/user/RegisterPage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,6 +18,8 @@ createRoot(document.getElementById("root")).render(
         <Route path="/fashion-blog" element={<FashionBlog />} />
         <Route path="/news" element={<NewsPage />} />
         <Route path="/users/login" element={<LoginPage />} />
+        <Route path="/users/chat" element={<ChatPage />} />
+        <Route path="/users/signup" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
