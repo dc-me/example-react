5da3f3f40dbdbd0f8a84001eebd154bb6c79e33a
dany <danychen.me@gmail.com>
2024-11-23 17:12:46 -0800

    handle form validation

--- src/components/user/RegisterPage.jsx
+++ src/components/user/RegisterPage.jsx
@@ -1,7 +1,24 @@
+import { useForm } from "react-hook-form";
 import TextField from "../TextField";
 import Button from "../Button";
+import preventDefault from "../../lib/utils";
+import { emailPattern } from "../../lib/regexes";
 
 export default function RegisterPage() {
+  const {
+    watch,
+    register,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const password = watch("password");
+
+  const submit = (formData) => {
+    console.log(formData);
+  };
+
   return (
     <div className="min-h-screen bg-gray-200 p-5">
       <div className="mx-auto grid max-w-screen-2xl grid-cols-1 gap-x-8 gap-y-8 lg:grid-cols-3 lg:p-20">
@@ -12,17 +29,69 @@ export default function RegisterPage() {
             share.
           </p>
         </div>
-        <form className="col-span-2 rounded-xl bg-white">
+        <form
+          onSubmit={handleSubmit(submit)}
+          className="col-span-2 rounded-lg bg-white ring-1 ring-gray-300"
+        >
           <div className="mx-8 my-6 grid grid-cols-1 gap-x-8 gap-y-6 md:grid-cols-6">
-            <TextField className="md:col-span-3" label="First name:" />
-            <TextField className="md:col-span-3" label="Last name:" />
-            <TextField className="md:col-span-2" label="Username:" />
-            <TextField className="md:col-span-4" label="Email:" />
-            <TextField className="md:col-span-3" label="Password:" />
-            <TextField className="md:col-span-3" label="Confirm password:" />
+            <TextField
+              {...register("name.firstName", {
+                required: "Please enter your first name",
+              })}
+              className="md:col-span-3"
+              label="First name:"
+              error={errors.name?.firstName}
+            />
+            <TextField
+              {...register("name.lastName", {
+                required: "Please enter your last name",
+              })}
+              className="md:col-span-3"
+              label="Last name:"
+              error={errors.name?.lastName}
+            />
+            <TextField
+              {...register("username", {
+                required: "Please enter your username",
+              })}
+              className="md:col-span-2"
+              label="Username:"
+              error={errors.username}
+            />
+            <TextField
+              {...register("email", {
+                required: "Please enter your email",
+                pattern: emailPattern,
+              })}
+              className="md:col-span-4"
+              label="Email:"
+              error={errors.email}
+            />
+            <TextField
+              {...register("password", {
+                required: "Please enter your password",
+              })}
+              className="md:col-span-3"
+              label="Password:"
+              type="password"
+              error={errors.password}
+            />
+            <TextField
+              type="password"
+              className="md:col-span-3"
+              label="Confirm password:"
+              {...register("confirmPassword", {
+                required: "Please confirm your password",
+                validate(value) {
+                  return value === password || "The passwords do not match";
+                },
+              })}
+              error={errors.confirmPassword}
+              onPaste={preventDefault}
+            />
           </div>
           <div className="mt-10 flex justify-end gap-4 border-t border-gray-300 p-4">
-            <Button className="rounded-lg" variant="contained">
+            <Button type="button" className="rounded-lg" variant="contained">
               Reset
             </Button>
             <Button className="rounded-lg" variant="contained">
